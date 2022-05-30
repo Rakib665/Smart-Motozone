b9b220e9f3489af4465db5889ce81f0eb241e934
Rakib665 <islammdr44@gmail.com>
2022-05-30 19:31:32 +0600

    create manage all order

--- src/Pages/Dashboard/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ManageAllOrders = () => {
+    const [allOrder, setAllOrder] = useState([])
+    useEffect(() => {
+        fetch('https://rocky-bayou-52722.herokuapp.com/allOrder')
+            .then(res => res.json())
+            .then(data => setAllOrder(data))
+    }, [])
     return (
-        <div>
-            <h2>manage all Orders</h2>
+        <div class="overflow-x-auto">
+            <table class="table w-full">
+                <thead>
+                    <tr>
+                        <th>Email</th>
+                        <th>Product Name</th>
+                        <th>Order Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        allOrder.map(order => <tr>
+                            <th>{order.userEmail}</th>
+                            <td>{order.partName}</td>
+                            <td>{order.quantity}</td>
+                        </tr>)
+                    }
+                    
+                </tbody>
+            </table>
         </div>
     );
 };
