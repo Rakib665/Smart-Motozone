361264e63ee89496174496acf6cc120dee560644
Rakib665 <islammdr44@gmail.com>
2022-05-31 00:47:25 +0600

    cors

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 const UserRow = ({user,refetch}) => {
   
     const makeAdmin = () => {
-        fetch(`http://localhost:5000/user/admin/${user?.email}`, {
+        fetch(`https://rocky-bayou-52722.herokuapp.com/user/admin/${user?.email}`, {
           method: 'PUT',
           headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
